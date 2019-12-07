6e90533976a5fe4999cc23cd506b174594294df7
Kazuya Ueoka <info@fromkk.info>
2019-12-07 12:43:01 +0900

    setup for fromKK

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -4,4 +4,4 @@
 # コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
 # docker run -i -t -v $(pwd):/book vvakame/review:4.0 /bin/bash
 
-docker run -t --rm -v $(pwd):/book vvakame/review:4.0 /bin/bash -ci "cd /book && ./setup.sh && REVIEW_CONFIG_FILE=$REVIEW_CONFIG_FILE npm run pdf"
+docker run -t --rm -v $(pwd):/book vvakame/review:4.0 /bin/bash -ci "cd /book && gem install bundler:1.17.2 && ./setup.sh && REVIEW_CONFIG_FILE=$REVIEW_CONFIG_FILE npm run pdf"
